165e7c729a6064e54ddd3194fd1d3fcf15c06822
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2025-01-20 03:33:40 +0200

    add AddTaskModalComponent and button to open modal in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppTaskListComponent } from './app-task-list/app-task-list.component';
+import { AddTaskModalComponent } from './add-task-modal/add-task-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppTaskListComponent],
+  imports: [RouterOutlet, AppTaskListComponent, MatIcon],
   template: `
     <app-app-task-list></app-app-task-list>
-
+    <button mat-fab color="primary" (click)="openAddTaskModal()">
+    <mat-icon>add</mat-icon>
+    </button>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'client';
+  constructor(private dialog: MatDialog) { }
+
+  openAddTaskModal(): void {
+    this.dialog.open(AddTaskModalComponent, {
+      width: '400px',
+    });
+
+
+  }
+
+
+
 }
